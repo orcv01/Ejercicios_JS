c9a39376e2c4b5c3de9f5b07441d8c79399e1e1c
UsuarioM <orcv01@gmail.com>
2023-06-14 13:06:34 +0200

    Feat Ejercicio_8

---
+++ Ejercicio_8.js
@@ -0,0 +1,11 @@
+/* Completa la función que tomando dos números como argumento devuelva el más alto.*/
+function sum(numberOne , numberTwo) {
+    if(numberOne >= numberTwo){
+        console.log("El número mas alto es = " + numberOne);
+     }else{
+        console.log("El número mas alto es = " + numberTwo);
+        }
+}
+
+sum(5,8);
+
