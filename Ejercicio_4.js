d8c280224f5d9f90b415b8534e86d05b382ca82b
UsuarioM <orcv01@gmail.com>
2023-06-14 10:37:45 +0200

    Feat Ejercicio_4

---
+++ Ejercicio_4.js
@@ -0,0 +1,7 @@
+/*1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.*/
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+let ind = avengers.indexOf('HULK');
+console.log(avengers[ind]);
+    
