2b5e113f02412b164c9b121f3cf93e8ba66e9b26
UsuarioM <orcv01@gmail.com>
2023-06-14 14:06:02 +0200

    Feat Ejercicio_10

---
+++ Ejercicio_10.js
@@ -0,0 +1,16 @@
+/*Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. Puedes usar este array para probar tu función: */
+
+var ac = 0;
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+    for (const num of param) {
+        ac += num;    
+    }
+}
+
+sumAll(numbers);
+console.log(ac);
+
