978ab3dddde79764b4944b315f2683c43ee10b45
UsuarioM <orcv01@gmail.com>
2023-06-16 12:36:24 +0200

    Refactor Ejercicio_9

--- Ejercicio_9.js
+++ Ejercicio_9.js
@@ -3,16 +3,11 @@ caso de que dos strings tenga la misma longitud deberá devolver el primero.
 */
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
 
-var i;
 var k;
 var text;
-var text_1;
 var len;
 var size;
-var size_1;
-var size_2 = 0;
-var aux;
-
+var aux = " ";
 
 len = avengers.length;
 
@@ -24,18 +19,9 @@ function findLongestWord(param) {
 for(k = 1;k < len;k++){
     text = avengers[k];
     findLongestWord(text);
-    size_1 = size;
-
-    for(i = 0;i < len;i++ - k){
-        text_1 = avengers[i];
-        findLongestWord(text_1);
-        size_2 = size;
-        
-        if(size_1 >= size_2){
-            aux = text_1;
-            
+    if(size > aux.length){
+        aux = text;
         } 
-    }
 }
 
 console.log(aux);
