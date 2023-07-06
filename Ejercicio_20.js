3782a8a8180a56d2445ca61815a22b0e1f99874e
UsuarioM <orcv01@gmail.com>
2023-07-06 13:11:20 +0200

    feature Ejercicios_JS

--- Ejercicio_20.js
+++ Ejercicio_20.js
@@ -5,26 +5,23 @@ Recuerda que puedes usar la función .includes() para comprobarlo.
 Puedes usar este array:*/
 
 const toys = [
-{id: 5, name: 'Buzz MyYear'},
-{id: 11, name: 'Action Woman'},
-{id: 23, name: 'Barbie Man'},
-{id: 40, name: 'El gato con Guantes'},
-{id: 40, name: 'El gato felix'}
-]
-
+  { id: 5, name: "Buzz MyYear" },
+  { id: 11, name: "Action Woman" },
+  { id: 23, name: "Barbie Man" },
+  { id: 40, name: "El gato con Guantes" },
+  { id: 40, name: "El gato felix" },
+];
 
 for (const toy in toys) {
-    if (toys.hasOwnProperty.call(toys, toy)) {
-        const element = toys[toy];
-        let valor = element.name;
-        //console.log(valor.includes('gato'));
+  if (toys.hasOwnProperty.call(toys, toy)) {
+    const element = toys[toy];
+    let valor = element.name;
 
-        if (valor.includes('gato')) {
-            
-            let ind = valor.indexOf('gato');
-            // valor.splice(ind, 1);
-            delete valor;
-        }
-        console.log(element);
+    if (valor.includes("gato")) {
+      let ind = valor.indexOf("gato");
+      delete toys;
+      //console.log(element);
     }
+    console.log(element);
+  }
 }
